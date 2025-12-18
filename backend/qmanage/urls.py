307d788676212ee3fe63272137d96e8189147291
Rahul Gaddipati <rahulgaddipati3@gmail.com>
2025-12-18 16:35:45 +0530

    Redirect / to reception UI

--- backend/qmanage/urls.py
+++ backend/qmanage/urls.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.shortcuts import redirect
+from django.urls import path
+
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("accounts/", include("django.contrib.auth.urls")),
+    path("", lambda request: redirect("/ui/issue/")),
+
     path("", include("core.urls")),  # <-- pulls all api routes from core/urls.py
 ]
