8a5405da110ff45be01dda7a94f5327797a1ead0
Rahul Gaddipati <rahulgaddipati3@gmail.com>
2025-12-18 17:05:04 +0530

    Run migrations on startup for Render

---
+++ start.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env sh
+set -e
+
+echo "Running migrations..."
+cd /app/backend
+python manage.py migrate --noinput
+
+echo "Starting server..."
+gunicorn qmanage.wsgi:application --bind 0.0.0.0:${PORT:-8000}
