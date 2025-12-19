262ceaa97a59bd0d475e280a752aeee9aaeba2a5
Rahul Gaddipati <rahulgaddipati3@gmail.com>
2025-12-19 10:25:58 +0530

    Run migrate and collectstatic on boot

--- start.sh
+++ start.sh
@@ -1,17 +1,12 @@
 #!/usr/bin/env sh
 set -e
 
-echo "Preparing DB..."
-cd /app/backend
-
-# IMPORTANT: wipe sqlite so migrations run clean on Render
-rm -f db.sqlite3
-
 echo "Running migrations..."
+cd /app/backend
 python manage.py migrate --noinput
 
 echo "Collecting static..."
 python manage.py collectstatic --noinput
 
 echo "Starting server..."
-gunicorn qmanage.wsgi:application --bind 0.0.0.0:${PORT:-8000}
+gunicorn qmanage.wsgi:application --bind 0.0.0.0:$PORT
